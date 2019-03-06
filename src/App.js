219f4e2ea43b5b1b04f45012c8bcd5ca935fc4ee
wjxalexander <wjxalexander@hotmail.com>
2019-03-06 12:06:50 +0800

    566

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component,Fragment } from "react";
+import {getDataStructure} from './dataFactory'
 import data from "./data.json";
 import styles from "./styles.scss"
-import ReactDOM from 'react-dom';
 
 class App extends Component {
   constructor(props) {
@@ -10,51 +10,15 @@ class App extends Component {
       data: data,
       treeData: null
     };
-    // this.myRef = React.createRef();
   }
-  getDataStructure(data) {
-    let copy = data.slice();
-    // 给每个对象赋爸爸值
-    copy.forEach(item => {
-      let number = item.moduleCode.toString().split("");
-      let length = number.length;
-      for (let i = length - 1; i >= 0; i--) {
-        let bit = number[i];
-        if (bit !== "0") {
-          number[i] = "0";
-          break;
-        }
-      }
-      item.parentId = parseInt(number.join(""));
-      item.checkState = false;
-      item.show = true;
-    });
-    function list_to_tree(list) {
-      let map = {},
-        roots = [];
-      for (let i = 0; i < list.length; i += 1) {
-        map[list[i].moduleCode] = i; // initialize the map,创建映射将每个ID和当前i 映射起来
-        list[i].children = []; // initialize the children
-      }
-      for (let i = 0; i < list.length; i += 1) {
-        let node = list[i]; //单独取出
-        //说明他有爸爸
-        // if you have dangling branches check that map[node.parentId] exists
-        node.parentId !== 0
-          ? list[map[node.parentId]].children.push(node)
-          : roots.push(node);
-      }
-      return roots;
-    }
-    return list_to_tree(data);
-  }
-  generateTree(Data) {
-    return (Data.map((item,index)=>{ 
+
+  generateTree(data) {
+    return (data.map((item,index)=>{ 
       if (item.children.length === 0) {
         return (
           <Fragment key={index}>
             <li>
-              <input type="checkbox" checked= {item.checkState}></input>
+              <input type="checkbox" checked= {item.checkState} onClick={()=>{this.handleClick(item,index)}}></input>
               <span>{item.moduleName}</span>
           </li>
           </Fragment>
@@ -63,22 +27,51 @@ class App extends Component {
         return (
           <Fragment key={index}>
           <li>
-            <span className="switch-open" onClick={()=>{this.toggle(data,index)}}></span>
-            <input type="checkbox" checked= {item.checkState}></input>
-            <span>{item.moduleName}</span>
-            {item.show ? <ul>{ this.generateTree(item.children)}</ul> :null}
+              <span className="switch-open" onClick={()=>{this.toggle(data,index)}}></span>
+              {/* 以下是父节点的checkbox */}
+              <input type="checkbox" checked= {item.checkState} onClick={()=>{this.handleClick(item,index)}}></input>
+              <span >{item.moduleName}</span>
+              {item.show ? <ul>{this.generateTree(item.children)}</ul> :null}
           </li>
           </Fragment>
         );
       }
     }))
   }
+
   toggle = (data,index)=>{
-    data[index].show = !data[index]
+    data[index].show = !data[index].show
     this.setState({treeData: this.state.treeData})
   }
+
+  handleChildrenState(item,state,e){
+    item.forEach(element => {
+      element.checkState =state
+      element.children.length===0 ? element.checkState = state : this.handleChildrenState(element.children,state)
+    });
+  }
+  monitorChildrenState(treeData){
+    treeData.forEach(item=>{
+      if(item.children.length === 0 ){
+      return item.checkState
+      }else{
+         let index = item.children.findIndex((el)=>!el.checkState)
+         index < 0 ?  this.monitorChildrenState(item.children) : item.checkState = false 
+      }
+      console.log(item.moduleName,item.checkState)
+
+    })
+  }
+
+  handleClick(item){
+    let {treeData} = this.state
+    item.checkState = !item.checkState
+    this.handleChildrenState(item.children,item.checkState)
+    this.monitorChildrenState(treeData)
+    this.setState({treeData:treeData})
+  }
   componentWillMount() {
-    let treeData = this.getDataStructure(this.state.data);
+    let treeData = getDataStructure(this.state.data);
     this.setState({ treeData: treeData });
   }
   render() {
@@ -86,7 +79,7 @@ class App extends Component {
     let ret = this.generateTree(treeData)
     return(
     <div className="App">
-      <ul className="container">     
+      <ul className="container" style={{display:"flex"}}>     
         {ret}
       </ul>
     </div>);
---
+++ src/dataFactory.js
@@ -0,0 +1,39 @@
+
+function getDataStructure(data) {
+    let copy = data.slice();
+    // 给每个对象赋爸爸值
+    copy.forEach(item => {
+      let number = item.moduleCode.toString().split("");
+      let length = number.length;
+      for (let i = length - 1; i >= 0; i--) {
+        let bit = number[i];
+        if (bit !== "0") {
+          number[i] = "0";
+          break;
+        }
+      }
+      item.parentId = parseInt(number.join(""));
+      item.checkState = false;
+      item.show = true;
+    });
+    function list_to_tree(list) {
+      let map = {},
+        roots = [];
+      for (let i = 0; i < list.length; i += 1) {
+        map[list[i].moduleCode] = i; // initialize the map,创建映射将每个ID和当前i 映射起来
+        list[i].children = []; // initialize the children
+      }
+      for (let i = 0; i < list.length; i += 1) {
+        let node = list[i]; //单独取出
+        //说明他有爸爸
+        // if you have dangling branches check that map[node.parentId] exists
+        node.parentId !== 0
+          ? list[map[node.parentId]].children.push(node)
+          : roots.push(node);
+      }
+      return roots;
+    }
+    return list_to_tree(data);
+  }
+  
+  export {getDataStructure}
